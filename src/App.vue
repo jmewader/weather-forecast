adab67d3db1285b4dd053d25aad6f9afc3e81883
jmewader <smelw@list.ru>
2024-02-11 01:11:20 +0300

    create app

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div className="container">
+    <div className="block">
+      <h1 class="block__title">Прогноз погоды</h1>
+      <p className="block__description">Узнай погоду в своем городе</p>
+
+      <div className="block__action">
+        <input className="block__input" type="text" placeholder="Введите город" />
+        <button className="block__btn">Узнать погоду</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  // components: {
+  //   HelloWorld
+  // }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1280px;
+  height: 100vh;
+  margin: 0 auto;
+}
+
+.block {
+  width: 100%;
+  max-width: 900px;
+  min-height: 400px;
+  border-radius: 30px;
+  padding: 50px 20px;
+  background-color: #070d0e;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+
+.block__title {
+  font-size: 36px;
+  margin: 0 0 16px;
+}
+
+.block__description {
+  font-size: 18px;
+}
+
+.block__action {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  margin: 40px 0 0;
+}
+
+.block__input {
+  background: transparent;
+  border: 2px solid rgba(110, 110, 110, 0.322);
+  border-radius: 6px;
+  outline: none;
+  min-height: 40px;
+  padding: 0 10px;
+  color: white;
+}
+
+.block__btn {
+  background: #ffae00;
+  min-height: 40px;
+  min-width: 150px;
+  padding: 0 20px;
+  border: none;
+  border-radius: 6px;
+  color: white;
+  font-weight: 500;
+  cursor: pointer;
+  transition: 0.25s;
+  font-size: 16px;
+  font-weight: 500;
+}
+
+.block__btn:hover {
+  background: rgb(0, 0, 0);
 }
 </style>
