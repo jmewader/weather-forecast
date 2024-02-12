8b6199567c95092573c8b41645307991f9a33e75
jmewader <smelw@list.ru>
2024-02-12 21:57:43 +0300

    add api

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,67 @@
+<script lang="ts">
+import axios, { AxiosResponse } from "axios";
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      city: "",
+      error: "",
+      info: null as AxiosResponse<any, any> | null,
+    };
+  },
+  computed: {
+    cityName(this: { city: string }) {
+      return this.city;
+    },
+  },
+  methods: {
+    getWeather() {
+      if (this.city.trim().length < 2) {
+        this.error = "Введите название больше двух символов";
+        return false;
+      }
+
+      this.error = "";
+
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&units=metric&appid=4ac85ff83534b44db19f8d1c08ec4fcf`)
+        .then((res: AxiosResponse<any, any>) => {
+          this.info = res;
+          console.log('info', this.info.data)
+        })
+        .catch((error: any) => {
+          console.error("Error fetching weather data:", error);
+        });
+
+        
+    },
+
+    
+  },
+});
+
+
+</script>
+
 <template>
   <div className="container">
     <div className="block">
       <h1 class="block__title">Прогноз погоды~</h1>
-      <p className="block__description">Узнай погоду в своем городе</p>
+      <p className="block__description">Узнай погоду в {{ city == "" ? "вашем городе" : cityName }}</p>
 
       <div className="block__action">
-        <input className="block__input" type="text" placeholder="Введите город" />
-        <button className="block__btn">Узнать погоду</button>
+        <input v-model="city" className="block__input" type="text" placeholder="Введите город" />
+        <button @click="getWeather()" v-if="city != ''" className="block__btn">Узнать погоду</button>
+        <button disabled v-else className="block__btn">Узнать погоду</button>
       </div>
+      <p class="block__error-text">{{ this.error }}</p>
+
+      <p v-show="info != null">{{ info }}</p>
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "App",
-  // components: {
-  //   HelloWorld
-  // }
-});
-</script>
 
 <style scoped>
 .container {
@@ -66,7 +106,7 @@ export default defineComponent({
 }
 
 .block__input {
-  background: transparent;
+  background-color: transparent;
   border: 2px solid rgba(110, 110, 110, 0.322);
   border-radius: 6px;
   outline: none;
@@ -76,7 +116,7 @@ export default defineComponent({
 }
 
 .block__btn {
-  background: #ffae00;
+  background-color: #ffae00;
   min-height: 40px;
   min-width: 150px;
   padding: 0 20px;
@@ -90,7 +130,16 @@ export default defineComponent({
   font-weight: 500;
 }
 
-.block__btn:hover {
-  background: rgb(0, 0, 0);
+.block__btn:disabled {
+  background-color: gray;
+  cursor: not-allowed;
+}
+
+:not(:disabled).block__btn:hover {
+  background-color: rgb(0, 0, 0);
+}
+
+.block__error-text {
+  color: rgb(168, 0, 0);
 }
 </style>
